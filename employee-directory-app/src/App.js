5b909689a04978d0682a48caecc84ccedc57be8c
Tre Scott <tres1396@gmail.com>
2021-06-07 22:09:21 -0400

    creating header function

--- employee-directory-app/src/App.js
+++ employee-directory-app/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Directory from "./components/Directory";
-import Employees from "./components/Employees";
+
 
 function App() {
   return (
---
+++ employee-directory-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>Employee Directory</h1>
+                <p>Use the search bar to narrow down your results.</p>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
