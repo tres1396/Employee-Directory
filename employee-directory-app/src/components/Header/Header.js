4d96df7c90e4076138909de2c2e5be19fd2e497f
Tre Scott <tres1396@gmail.com>
2021-06-08 17:07:18 -0400

    adding search input

--- employee-directory-app/src/components/Header/Header.js
+++ employee-directory-app/src/components/Header/Header.js
@@ -2,14 +2,28 @@ import React from "react";
 import "./style.css";
 
 function Header() {
-    return (
-        <div>
-            <header>
-                <h1>Employee Directory</h1>
-            </header>
-            <p>Use the search bar to narrow down your results.</p>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <h1>Employee Directory</h1>
+      </header>
+      <div className="subheader">
+        <p>Use the search bar to narrow down your results.</p>
+      </div>
+      <div className="search">
+      <div className="search-bar">
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Search"
+        ></input>
+      </div>
+      </div>
+      {/* closing div */}
+    </div>
+  );
 }
 
 export default Header;
